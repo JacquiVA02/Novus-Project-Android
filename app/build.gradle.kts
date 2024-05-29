a9da0a610a161e78ba16dd78b69cb6b4b75a814c
JacquiVA02 <a01736339@tec.mx>
2024-05-28 22:42:28 -0600

    Nuevas Interfaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
-
+    implementation("com.google.firebase:firebase-auth:23.0.0")
     // Add the dependencies for any other desired Firebase products
     // https://firebase.google.com/docs/android/setup#available-libraries
 }
