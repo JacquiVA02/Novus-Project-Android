9e8586267ba62b3fde3ef2c4db9131be26b54644
JacquiVA02 <a01736339@tec.mx>
2024-05-28 18:40:21 -0600

    Navegacion inicio

--- app/src/main/java/com/example/novusproject/MainActivity.java
+++ app/src/main/java/com/example/novusproject/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.novusproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +15,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botonInicio, botonRegistro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Referencia a botones usando el ID
+        botonInicio = findViewById(R.id.button_inicio);
+        botonRegistro = findViewById(R.id.button_registro);
+
+        botonInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SesionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/novusproject/SesionActivity.java
+++ app/src/main/java/com/example/novusproject/SesionActivity.java
@@ -1,6 +1,9 @@
 package com.example.novusproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SesionActivity extends AppCompatActivity {
 
+    private ImageView botonAtras;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sesion);
+
+        botonAtras = findViewById(R.id.buttonBackSesion);
+
+        botonAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SesionActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
