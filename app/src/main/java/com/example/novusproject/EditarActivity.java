8b7e5c502ab96e294ba4d34c28c9f8a18f8b5b3a
vanech20 <estefyvane2007@gmail.com>
2024-06-20 17:17:18 -0600

    Intento 1: Editar avatar

--- app/src/main/java/com/example/novusproject/EditarActivity.java
+++ app/src/main/java/com/example/novusproject/EditarActivity.java
@@ -1,7 +1,10 @@
 package com.example.novusproject;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,9 +14,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
 public class EditarActivity extends AppCompatActivity {
 
     ImageView btn_hat, btn_glasses, btn_shirt, btn_shoes, btn_back;
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,22 @@ public class EditarActivity extends AppCompatActivity {
         btn_glasses = findViewById(R.id.imageViewGlasses);
         btn_shirt = findViewById(R.id.imageViewShirt);
         btn_shoes = findViewById(R.id.imageViewShoe);
+
+        // Inicialización de Firebase
+        db = FirebaseFirestore.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+        // Verificar el usuario actual
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            Log.d(TAG, "Usuario ID: " + userId);
+        } else {
+            Log.d(TAG, "Usuario no autenticado");
+            startActivity(new Intent(EditarActivity.this, SesionActivity.class));
+            finish();
+            return;
+        }
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,5 +108,38 @@ public class EditarActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        verificarVestimenta();
     }
+
+
+    private void verificarVestimenta() {
+        // Se obtiene al usuario actual
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+
+            // Referenciar al documento de UsuarioIsla
+            DocumentReference userDocRef = db.collection("UsuarioAvatar").document(userId);
+
+            // Escuchar cambios en el documento en tiempo real
+            userDocRef.addSnapshotListener((documentSnapshot, e) -> {
+                if (e != null) {
+                    Log.d(TAG, "Error al obtener el documento", e);
+                    return;
+                }
+
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    // El documento existe, obtener sus datos
+                    Map<String, Object> userData = documentSnapshot.getData();
+
+                } else {
+                    Log.d(TAG, "El documento no existe.");
+                }
+            });
+        } else {
+            Log.d(TAG, "Usuario no autenticado");
+        }
+    }
+
 }
